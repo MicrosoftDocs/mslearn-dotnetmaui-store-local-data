f11de4b99b69e9fbac236e6bb7791a2de26619d3
Matt Soucoup <msoucoup@codemilltech.com>
2022-05-13 09:23:25 -0700

    More initial setup code

--- People/People/MainPage.xaml.cs
+++ People/People/MainPage.xaml.cs
@@ -4,16 +4,27 @@ namespace People;
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
 
 	public MainPage()
 	{
 		InitializeComponent();
 	}
 
-	private void OnCounterClicked(object sender, EventArgs e)
-	{
+    public void OnNewButtonClicked(object sender, EventArgs args)
+    {
+        statusMessage.Text = "";
 
-	}
+        App.PersonRepo.AddNewPerson(newPerson.Text);
+        statusMessage.Text = App.PersonRepo.StatusMessage;
+    }
+
+    public void OnGetButtonClicked(object sender, EventArgs args)
+    {
+        statusMessage.Text = "";
+
+        List<Person> people = App.PersonRepo.GetAllPeople();
+        peopleList.ItemsSource = people;
+    }
+
 }
 
---
+++ People/People/PersonRepository.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace People
+{
+    public class PersonRepository
+    {
+        public string StatusMessage { get; set; }
+
+        // TODO: Add variable for connection
+
+        public PersonRepository(string dbPath)
+        {
+            // TODO: Add code to initialize the repository
+        }
+
+        public void AddNewPerson(string name)
+        {
+            int result = 0;
+            try
+            {
+                //basic validation to ensure a name was entered
+                if (string.IsNullOrEmpty(name))
+                    throw new Exception("Valid name required");
+
+                // TODO: Insert the new person into the database
+
+                StatusMessage = string.Format("{0} record(s) added [Name: {1})", result, name);
+            }
+            catch (Exception ex)
+            {
+                StatusMessage = string.Format("Failed to add {0}. Error: {1}", name, ex.Message);
+            }
+
+        }
+
+        public List<Person> GetAllPeople()
+        {
+            // TODO: Retrieve a list of Person objects from the database into a list
+        }
+    }
+}
