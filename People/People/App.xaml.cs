a05c3bd1ee1eb40153c5494ba8e0779ad787dd2b
Matt Soucoup <msoucoup@codemilltech.com>
2022-05-13 09:06:36 -0700

    Initial load

---
+++ People/People/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace People;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ People/People/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿
+
+namespace People;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+
+	}
+}
+
