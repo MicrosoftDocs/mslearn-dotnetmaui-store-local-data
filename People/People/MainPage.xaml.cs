d738af7367411c14c5b23597b00d5a4c79315d92
v-thpra <v-thpr@microsoft.com>
2022-06-20 16:23:36 -0700

    fix typos and async

--- People/People/MainPage.xaml.cs
+++ People/People/MainPage.xaml.cs
@@ -11,19 +11,19 @@ public partial class MainPage : ContentPage
 		InitializeComponent();
 	}
 
-    public async void OnNewButtonClicked(object sender, EventArgs args)
+    public void OnNewButtonClicked(object sender, EventArgs args)
     {
         statusMessage.Text = "";
 
-        await App.PersonRepo.AddNewPerson(newPerson.Text);
+        App.PersonRepo.AddNewPerson(newPerson.Text);
         statusMessage.Text = App.PersonRepo.StatusMessage;
     }
 
-    public async void OnGetButtonClicked(object sender, EventArgs args)
+    public void OnGetButtonClicked(object sender, EventArgs args)
     {
         statusMessage.Text = "";
 
-        List<Person> people = await App.PersonRepo.GetAllPeople();
+        List<Person> people = App.PersonRepo.GetAllPeople();
         peopleList.ItemsSource = people;
     }
 
--- People/People/PersonRepository.cs
+++ People/People/PersonRepository.cs
@@ -31,7 +31,7 @@ namespace People
             {
                 // TODO: Call Init()
 
-                //basic validation to ensure a name was entered
+                // basic validation to ensure a name was entered
                 if (string.IsNullOrEmpty(name))
                     throw new Exception("Valid name required");
 
@@ -47,7 +47,7 @@ namespace People
 
         }
 
-        public <List<Person> GetAllPeople()
+        public List<Person> GetAllPeople()
         {
             // TODO: Init then retrieve a list of Person objects from the database into a list
             try
