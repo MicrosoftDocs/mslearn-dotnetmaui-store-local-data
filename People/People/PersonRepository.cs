dae164433172a2e54c28204096435a6b276123fd
James Montemagno <james.montemagno@gmail.com>
2023-10-20 11:46:56 -0700

    project cleanup

--- People/People/PersonRepository.cs
+++ People/People/PersonRepository.cs
@@ -1,65 +1,58 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace People;
 
-namespace People
+public class PersonRepository
 {
-    public class PersonRepository
+    string _dbPath;
+
+    public string StatusMessage { get; set; }
+
+    // TODO: Add variable for the SQLite connection
+
+    private void Init()
     {
-        string _dbPath;
+        // TODO: Add code to initialize the repository         
+    }
 
-        public string StatusMessage { get; set; }
+    public PersonRepository(string dbPath)
+    {
+        _dbPath = dbPath;                        
+    }
 
-        // TODO: Add variable for the SQLite connection
-
-        private void Init()
+    public void AddNewPerson(string name)
+    {            
+        int result = 0;
+        try
         {
-            // TODO: Add code to initialize the repository         
-        }
+            // TODO: Call Init()
 
-        public PersonRepository(string dbPath)
+            // basic validation to ensure a name was entered
+            if (string.IsNullOrEmpty(name))
+                throw new Exception("Valid name required");
+
+            // TODO: Insert the new person into the database
+            result = 0;
+
+            StatusMessage = string.Format("{0} record(s) added (Name: {1})", result, name);
+        }
+        catch (Exception ex)
         {
-            _dbPath = dbPath;                        
+            StatusMessage = string.Format("Failed to add {0}. Error: {1}", name, ex.Message);
         }
 
-        public void AddNewPerson(string name)
-        {            
-            int result = 0;
-            try
-            {
-                // TODO: Call Init()
+    }
 
-                // basic validation to ensure a name was entered
-                if (string.IsNullOrEmpty(name))
-                    throw new Exception("Valid name required");
-
-                // TODO: Insert the new person into the database
-                result = 0;
-
-                StatusMessage = string.Format("{0} record(s) added (Name: {1})", result, name);
-            }
-            catch (Exception ex)
-            {
-                StatusMessage = string.Format("Failed to add {0}. Error: {1}", name, ex.Message);
-            }
-
-        }
-
-        public List<Person> GetAllPeople()
+    public List<Person> GetAllPeople()
+    {
+        // TODO: Init then retrieve a list of Person objects from the database into a list
+        try
         {
-            // TODO: Init then retrieve a list of Person objects from the database into a list
-            try
-            {
-                
-            }
-            catch (Exception ex)
-            {
-                StatusMessage = string.Format("Failed to retrieve data. {0}", ex.Message);
-            }
-
-            return new List<Person>();
+            
         }
+        catch (Exception ex)
+        {
+            StatusMessage = string.Format("Failed to retrieve data. {0}", ex.Message);
+        }
+
+        return new List<Person>();
     }
 }
